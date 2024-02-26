05bce4a78dc28215f0ea21e3d1d46ea47f2f0f90
afreyes <afragredo@gmail.com>
2024-02-26 18:13:09 -0500

    rutas

--- components/pages/estadistica/verDetalle.jsx
+++ components/pages/estadistica/verDetalle.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function verDetalle() {
   return (
       <>
-          <Image/>
+        <Image src="./"></Image>
          </>
   )
 }
--- src/app/organizador/campeonatos/estadisticas/manana/page.jsx
+++ src/app/organizador/campeonatos/estadisticas/manana/page.jsx
@@ -2,19 +2,18 @@ import { PageEstadisticas } from '../../../../../../components/pages/estadistica
 import React from 'react'
 import '@/styles/styleEstadisticas/styleManana.module.css'
 import RootLayout from '@/app/layout'
-import SideCampeonato from '@/Components/sideCampeonato'
+
 
 export default function Manana() {
   return (
     <>
-      <RootLayout>
+   
 
-        <SideCampeonato></SideCampeonato>
         <div >
           <h1 className='jornada'>Mañana</h1>
           <PageEstadisticas />
         </div>
-      </RootLayout>
+   
     </>
   )
 }
