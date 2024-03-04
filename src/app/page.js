02ef95f2aba1cfe17789c08f6e4d8a7cf609593d
brayan23 <bdelgadomunoz@gmail.com>
2024-03-04 17:09:05 -0500

    ajuste page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import ListadoIntegrantes from "../../components/pages/componenteParticipantes/pageListadoIntegrantes";
-import CampeonatosDisponibles from "../../components/pages/dashjugador/campeonatosDisponibles";
 
 export default function Home() {
   return (
     <>
-   <CampeonatosDisponibles></CampeonatosDisponibles>
+
     </>
   );
 } 
