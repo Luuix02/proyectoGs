efd11d174775ff846508db9a03b948ee3fcfd477
Luisa Villacorte <lufvi02@gmail.com>
2024-03-06 10:38:57 -0500

    Actualizando error client

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,3 @@
-
-// import ListadoIntegrantes from "../../components/pages/componenteParticipantes/pageListadoIntegrantes";
-
-
-// export default function Home() {
-//   return (
-//     <>
-//    <ListadoIntegrantes/>
-//     </>
-//   );
-// } 
-
 export default function Home() {
   return (
     <>
