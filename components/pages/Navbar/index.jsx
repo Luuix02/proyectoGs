18dc4a995f848fc337f2c177a550802452af2378
Luuix02 <Lufvi02@gmail.com>
2024-03-11 13:41:19 -0500

    actualizaciones rutas etc

--- components/pages/Navbar/index.jsx
+++ components/pages/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import '../../../src/styles/StyleNavbar/main.css'
 export default function Header() {
+    const path = '/auth'
 
     return (
         <>
@@ -10,10 +11,12 @@ export default function Header() {
                         <img src="" alt="Logo" />
                     </div>
                     <div class="links">
-                        <a href="">Iniciar sesión</a>
-                        <a href="">Regristrarse</a>
+                        <Link href={`${path}/registro`}>Registrarse</Link>
+                        <Link href={`${path}/inicioSesion`}>Iniciar sesión</Link>
                         <a href="">Equipo Bienestar</a>
                         <a href="">Contactanos</a>
+                       
+                        <a></a>
                     </div>
                 </div>
             </nav>
