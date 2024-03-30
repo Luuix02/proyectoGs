736cfc91916a70a34edf9af639c7554812fd75fe
Luisa Fernanda <lufvi02@gmail.com>
2024-03-29 22:52:46 -0500

    actualizaciones layout, siberar, pageCampeonato y estilos

--- components/common/sidebar/page.jsx
+++ components/common/sidebar/page.jsx
@@ -1,38 +1,53 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import '../../../src/styles/styleSidebar/styleSidebar.css'
+import Link from "next/link";
+import Image from "next/image";
+import "../../../src/styles/styleSidebar/styleSidebar.css";
 
 export default function SideBarPrincipal() {
-   
-    const pathImg = '/images/IconosSide'
+  const pathImg = "/images/IconosSide";
   return (
     <aside className="barraLateral">
-      <div className='headerAside'>
-      <Image src={`${pathImg}/menu.png`}className='imgH' width={35} height={35}></Image>
-      <h1 className='textHeader'>Administrador</h1>
-      </div>
-        <ul className="lista">
-          <li className={`contenedorAside `}>
-            <Image src={`${pathImg}/campeonatos.png`}className='img' width={35} height={35}></Image>
-          <Link href={`campeonatos`} className='li'>Campeonatos </Link>
-          </li>
-          <li className='contenedorAside'>
-            <Image src={`${pathImg}/crearCampenato.png`} className='img2' width={35} height={35}></Image>
-            <Link href={`crearCampeonato`} className='li'>Crear Campeonatos</Link>
-          </li>
-          <li className='contenedorAside'>
-            <Image src={`${pathImg}/fotos.png`} width={35} height={35}></Image>
-            <Link href={`fotos`} className='li'>Fotos</Link>
-          </li>
-          <li className='contenedorAside'>
-            <Image src={`${pathImg}/cuenta.png`} width={35} height={35}></Image>
-            <Link href={`perfil`} className='li'>Cuenta</Link>
-          </li>
-          <li className='contenedorAside'>
-            <Image src={`${pathImg}/salir.png`} width={35} height={35}></Image>
-          <Link href={`/`} className='li'>Salir</Link>
-          </li >
-          </ul>
-      </aside>
-  )
+      <ul className="lista">
+        <li className={`contenedorAside `}>
+          <Image
+            src={`${pathImg}/campeonatos.png`}
+            className="img"
+            width={35}
+            height={35}
+          ></Image>
+          <Link href={`campeonatos`} className="li">
+            Campeonatos{" "}
+          </Link>
+        </li>
+        <li className="contenedorAside">
+          <Image
+            src={`${pathImg}/crearCampenato.png`}
+            className="img2"
+            width={35}
+            height={35}
+          ></Image>
+          <Link href={`crearCampeonato`} className="li">
+            Crear Campeonatos
+          </Link>
+        </li>
+        <li className="contenedorAside">
+          <Image src={`${pathImg}/fotos.png`} width={35} height={35}></Image>
+          <Link href={`fotos`} className="li">
+            Fotos
+          </Link>
+        </li>
+        <li className="contenedorAside">
+          <Image src={`${pathImg}/cuenta.png`} width={35} height={35}></Image>
+          <Link href={`perfil`} className="li">
+            Cuenta
+          </Link>
+        </li>
+        <li className="contenedorAside">
+          <Image src={`${pathImg}/salir.png`} width={35} height={35}></Image>
+          <Link href={`/`} className="li">
+            Salir
+          </Link>
+        </li>
+      </ul>
+    </aside>
+  );
 }
