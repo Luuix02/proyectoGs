954b788699637bcf1cb5d2d184fd9c96a0898330
Luisa Fernanda <lufvi02@gmail.com>
2024-07-09 21:28:26 -0500

    layout proteccion rutas en proceso

---
+++ components/layout/organizadorLayout.jsx
@@ -0,0 +1,12 @@
+// import Cookies from 'js-cookie'
+// import React from 'react'
+// import { useEffect, useState } from 'react'
+
+// export default function organizadorLayout() {
+// const [token, setToken] = useState(Cookies.get("token"));
+
+
+//   return (
+//     <div>organizadorLayout</div>
+//   )
+// }
--- components/pages/componenteRegistro/page.jsx
+++ components/pages/componenteRegistro/page.jsx
@@ -1,150 +1,97 @@
 'use client'
-import React, { useState } from "react";
-import Link from "next/link";
-import "../../../src/styles/styleRegistro/styleRegistro.css";
-import RootLayout from "@/app/layout";
-import { useRouter } from "next/router";
-import { registroUser } from "@/lib/api";
+import { registroUser } from '@/lib/api';
+import '../../../src/styles/styleRegistro/styleRegistro.css'
 
-export default function Registro() {
- 
-  const path = "/auth";
-
-  const [datosRegistro, setDatosRegistro] = useState({
-    correo: "",
-    contrasena: "",
-    nombres: "",
-    telefono: "",
-    identificacion: "",
-    programa: "",
-    fichaFin: "",
-    ficha: "",
-    confirmarContrasena: '',
-  });
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await registroUser(datosRegistro);
-      console.log("usario registrado", response);
-    } catch (error) {
-      console.log("Errro al registrar usuario", error.message);
+export default function RegisterUser() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = async (data) => {
+    try{
+      await registroUser(data);
+      alert('Usuario registrado correctamente');
+    } catch (error){
+      alert (error.message);
     }
   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setDatosRegistro((prevDatos) => ({
-      ...prevDatos,
-      [name]: value,
-    }));
-  };
-
-
-return (
-  <>
-    
-      <body className="FONDO  ">
-        <div className="mayor class">
-          <h1 className="titulo">Regístrate en GoSport</h1>
-          <br />
-          <div className="divdatos2">
-            <div className="divdatos">
-              <form onSubmit={handleSubmit} action="datos">
-                <label htmlFor="nombres">Nombres</label>
-                <input
-                  type="text"
-                  id="nombres"
-                  name="nombres"
-                  value={datosRegistro.nombres}
-                  onChange={handleChange}
-                  placeholder="ejemplo juan perez"
-                  required
-                />
-
-
-                <label htmlFor="telefono">Teléfono</label>
-                <input
-                  className="la"
-                  type="tel"
-                  id="telefono"
-                  name="telefono"
-                  value={datosRegistro.telefono}
-                  onChange={handleChange}
-                  placeholder="ej:3163221523"
-                  required
-                />
-
-
-
-{/* agregar los campos de identificacion, fin fecha y programa*/}
-
-
-                <label htmlFor="ficha">N° Ficha</label>
-                <input
-                  className="la"
-                  type="  tel"
-                  id="ficha"
-                  name="ficha"
-                  value={datosRegistro.ficha}
-                  onChange={handleChange}
-                  placeholder="ej:2669739"
-                  required
-                />
-
-                <br />
-                <label htmlFor="correo">Email</label>
-                <input
-                  type="email"
-                  id="correo"
-                  name="correo"
-                  value={datosRegistro.correo}
-                  onChange={handleChange}
-                  placeholder="ej:pepito@gmail.com"
-                  required
-                />
-
-                <label htmlFor="contrasena">Contraseña</label>
-                <input
-                  type="password"
-                  id="contrasena"
-                  name="contrasena"
-                  value={datosRegistro.contrasena}
-                  onChange={handleChange}
-                  placeholder="***********"
-                  required
-                />
-
-                <label htmlFor="confirmar-password">Confirmar contraseña</label>
-                <input
-                  type="password"
-                  id="confirmarContrasena"
-                  name="confirmarContrasena"
-                  value={datosRegistro.confirmarContrasena}
-                  onChange={handleChange}
-                  placeholder="***********"
-                  required
-                />
-
-                
-                  <button type="submit" className="Botonregistros">Registrarse</button>
-               
-
-                <p className="cuenta">
-                  ¿Ya tienes cuenta?{" "}
-                  <Link
-                    href={`${path}/inicioSesion`}
-                    className="parairaotrapagina"
-                  >
-                    Inicia Sesión
-                  </Link>
-                </p>
-              </form>
+  return (
+    <div className="contenedorPadreRegisro">
+      <div className="contenedorFormularioReg">
+        <h1 className="tituloPrincipal">Regístrate en GoSport</h1>
+        <h2 className="tituloSecundario">Queremos saber un poco de ti, por favor llena el formulario completo</h2>
+        <form>
+          <div className="form-row">
+            <div className="form-group">
+              <input
+                type="text"
+                id="nombres"
+                placeholder="Ingresa tus nombres completos"
+                className="campoDato"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                id="telefono"
+                placeholder="Ingresa tu número de celular"
+                className="campoDato"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                id="identificacion"
+                placeholder="Ingresa tus número de identificación"
+                className="campoDato"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                id="programa"
+                placeholder="Ingresa el programa que estás cursando"
+                className="campoDato"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                id="ficha"
+                placeholder="Ingresa tu número de ficha"
+                className="campoDato"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="date"
+                id="finFicha"
+                placeholder="Ingresa fecha de terminación del programa"
+                className="campoDato"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                id="correo"
+                placeholder="Ingresa tu correo electrónico"
+                className="campoDato"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                id="contrasena"
+                placeholder="Crea tu contraseña"
+                className="campoDato"
+              />
             </div>
           </div>
-        </div>
-      </body>
-   
-  </>
-);
-};
+          <div className="form-group-btn">
+            <button className="Registrar">
+              Registrarse
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
