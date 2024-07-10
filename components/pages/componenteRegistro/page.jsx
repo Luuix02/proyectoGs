4f06c9574e50090b7d5495760dda09174d5c8c41
Luisa Fernanda <lufvi02@gmail.com>
2024-07-09 23:56:44 -0500

    form registro actualizado

--- components/pages/componenteRegistro/page.jsx
+++ components/pages/componenteRegistro/page.jsx
@@ -1,15 +1,26 @@
-'use client'
-import { registroUser } from '@/lib/api';
-import '../../../src/styles/styleRegistro/styleRegistro.css'
+"use client";
+import { registroUser } from "@/lib/api";
+import { useForm } from "react-hook-form";
+import "../../../src/styles/styleRegistro/styleRegistro.css";
+import { useState } from "react";
 
 export default function RegisterUser() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
   const onSubmit = async (data) => {
-    try{
-      await registroUser(data);
-      alert('Usuario registrado correctamente');
-    } catch (error){
-      alert (error.message);
+    data.finFicha = new Date(data.finFicha).toISOString();
+    try {
+      const response = await registroUser(data);
+      alert("Usuario registrado correctamente");
+      reset();
+    } catch (error) {
+      console.error("Error en el registro del usuario:", error.message);
+      alert(error.message);
     }
   };
 
@@ -17,8 +28,10 @@ export default function RegisterUser() {
     <div className="contenedorPadreRegisro">
       <div className="contenedorFormularioReg">
         <h1 className="tituloPrincipal">Regístrate en GoSport</h1>
-        <h2 className="tituloSecundario">Queremos saber un poco de ti, por favor llena el formulario completo</h2>
-        <form>
+        <h2 className="tituloSecundario">
+          Queremos saber un poco de ti, por favor llena el formulario completo
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-row">
             <div className="form-group">
               <input
@@ -26,7 +39,11 @@ export default function RegisterUser() {
                 id="nombres"
                 placeholder="Ingresa tus nombres completos"
                 className="campoDato"
+                {...register("nombres", {
+                  required: "Este campo es obligatorio",
+                })}
               />
+              {errors.nombres && <span>{errors.nombres.message}</span>}
             </div>
             <div className="form-group">
               <input
@@ -34,7 +51,11 @@ export default function RegisterUser() {
                 id="telefono"
                 placeholder="Ingresa tu número de celular"
                 className="campoDato"
+                {...register("telefono", {
+                  required: "Este campo es obligatorio",
+                })}
               />
+              {errors.telefono && <span>{errors.telefono.message}</span>}
             </div>
             <div className="form-group">
               <input
@@ -42,7 +63,13 @@ export default function RegisterUser() {
                 id="identificacion"
                 placeholder="Ingresa tus número de identificación"
                 className="campoDato"
+                {...register("identificacion", {
+                  required: "Este campo es obligatorio",
+                })}
               />
+              {errors.identificacion && (
+                <span>{errors.identificacion.message}</span>
+              )}
             </div>
             <div className="form-group">
               <input
@@ -50,7 +77,11 @@ export default function RegisterUser() {
                 id="programa"
                 placeholder="Ingresa el programa que estás cursando"
                 className="campoDato"
+                {...register("programa", {
+                  required: "Este campo es obligatorio",
+                })}
               />
+              {errors.programa && <span>{errors.programa.message}</span>}
             </div>
             <div className="form-group">
               <input
@@ -58,7 +89,11 @@ export default function RegisterUser() {
                 id="ficha"
                 placeholder="Ingresa tu número de ficha"
                 className="campoDato"
+                {...register("ficha", {
+                  required: "Este campo es obligatorio",
+                })}
               />
+              {errors.ficha && <span>{errors.ficha.message}</span>}
             </div>
             <div className="form-group">
               <input
@@ -66,7 +101,11 @@ export default function RegisterUser() {
                 id="finFicha"
                 placeholder="Ingresa fecha de terminación del programa"
                 className="campoDato"
+                {...register("finFicha", {
+                  required: "Este campo es obligatorio",
+                })}
               />
+              {errors.finFicha && <span>{errors.finFicha.message}</span>}
             </div>
             <div className="form-group">
               <input
@@ -74,7 +113,11 @@ export default function RegisterUser() {
                 id="correo"
                 placeholder="Ingresa tu correo electrónico"
                 className="campoDato"
+                {...register("correo", {
+                  required: "Este campo es obligatorio",
+                })}
               />
+              {errors.correo && <span>{errors.correo.message}</span>}
             </div>
             <div className="form-group">
               <input
@@ -82,16 +125,20 @@ export default function RegisterUser() {
                 id="contrasena"
                 placeholder="Crea tu contraseña"
                 className="campoDato"
+                {...register("contrasena", {
+                  required: "Este campo es obligatorio",
+                })}
               />
+              {errors.contrasena && <span>{errors.contrasena.message}</span>}
             </div>
           </div>
           <div className="form-group-btn">
-            <button className="Registrar">
+            <button type="submit" className="Registrar">
               Registrarse
             </button>
           </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
